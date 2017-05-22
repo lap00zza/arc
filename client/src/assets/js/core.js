75511a35dbb4b601393213de1a7f220990f359bc
Lapoozza <jewelmahanta@gmail.com>
2017-05-22 18:58:16 +0530

    ref (*): added license and readme

--- client/src/assets/js/core.js
+++ client/src/assets/js/core.js
@@ -1,3 +1,23 @@
+/**
+ *  arc - dead simple chat
+ *  Copyright (C) 2017 Jewel Mahanta <jewelmahanta@gmail.com>
+ *
+ *  This file is part of arc.
+ *
+ *  arc is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  arc is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with arc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var ws_url = "ws://" + window.location.hostname + "/ws";
 var ws = new WebSocket(ws_url);
 var vm = document.getElementById("viewMessage");
--- ws_server/run.py
+++ ws_server/run.py
@@ -1,3 +1,23 @@
+"""
+arc - dead simple chat
+Copyright (C) 2017 Jewel Mahanta <jewelmahanta@gmail.com>
+
+This file is part of arc.
+
+arc is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+arc is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with arc.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import asyncio
 import websockets
 import json
@@ -33,6 +53,5 @@ async def handler(websocket, path):
         print(connected)
 
 start_server = websockets.serve(handler, "0.0.0.0", 5555)
-
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
