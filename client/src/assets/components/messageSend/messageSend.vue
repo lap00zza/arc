b75b829242266fb3327e47eb0afacadd9fca3ea0
Lapoozza <jewelmahanta@gmail.com>
2017-05-26 19:28:08 +0530

    ref  (client, sendMessage): almost vertically centered the textarea

--- client/src/assets/components/messageSend/messageSend.vue
+++ client/src/assets/components/messageSend/messageSend.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="message-send">
         <div class="widget">
-            <div class="text">
-                <textarea rows="1" v-model="sendMessageInput"
-                          v-on:input="onInput" v-on:keydown="onKeyDown"
-                          placeholder="Message #general"></textarea>
-            </div>
+            <textarea rows="1" v-model="sendMessageInput"
+                      v-on:input="onInput" v-on:keydown="onKeyDown"
+                      placeholder="Message #general"></textarea>
             <div class="emojis"></div>
         </div>
         <div class="meta">press <code>enter</code> to send message</div>
