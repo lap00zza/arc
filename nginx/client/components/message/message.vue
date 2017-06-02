a597686d5b73a1148092e5d19944ae4d080b11d8
Lapoozza <jewelmahanta@gmail.com>
2017-06-03 00:14:51 +0530

    ref (message): Now message displays avatar and username

--- nginx/client/components/message/message.vue
+++ nginx/client/components/message/message.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="message" v-once>
         <div class="profile_image">
-            <img src="/assets/images/user_place_holder.png">
+            <img v-bind:src="avatar">
         </div>
         <div class="message_body">
-            <div class="author">Anonymous</div>
+            <div class="author">{{ message.author.username }}</div>
             <div class="content">{{ message.content }}</div>
         </div>
     </div>
 </template>
 <script>
     export default {
-        props: ["message"]
+        props: ["message"],
+        computed: {
+            avatar: function () {
+                return "https://www.gravatar.com/avatar/" + this.$props.message.author.avatar
+            }
+        }
     }
 </script>
