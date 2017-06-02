66b68399834667b913185d5ebdb8e555434e73b2
Lapoozza <jewelmahanta@gmail.com>
2017-06-03 00:16:17 +0530

    ref (store): added myInfo to state
    This can be used to quickly access the user information (username, user_id, avatar etc.)

--- nginx/client/store/index.js
+++ nginx/client/store/index.js
@@ -4,12 +4,39 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 const state = {
-    messages: []
+    messages: [],
+    myInfo: {
+        id: "",
+        username: "",
+        avatar: ""
+    }
 };
 
 const mutations = {
     addMessage: function (state, message) {
         state.messages.push(message)
+    },
+    /**
+     * This method can be used to independently update myInfo or 
+     * update them all at once.
+     * 
+     * For updating independently, make sure to pass Null to the 
+     * other params.
+     * 
+     * @param state {Object}
+     * @param info {Object}
+     */
+    updateMyInfo: function (state, info) {
+        if (info.id) {
+            state.myInfo.id = info.id;
+        }
+        if (info.username) {
+            state.myInfo.username = info.username;
+        }
+        if (info.avatar) {
+            state.myInfo.avatar = info.avatar;
+        }
+        
     }
 };
 
