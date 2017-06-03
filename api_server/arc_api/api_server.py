ecbc4d24a31f2e415da7ec0dd6b146805e52f62c
Lapoozza <jewelmahanta@gmail.com>
2017-06-03 22:56:41 +0530

    ref (*): Major Refactor
    The API Server is almost DRY now :smile:

--- api_server/arc_api/api_server.py
+++ api_server/arc_api/api_server.py
@@ -29,8 +29,7 @@ from .auth import Auth
 from .database import DB, NoUserError, LengthError, ValidationError
 
 
-# TODO: Use a message broker (REDIS or rabbitMQ) for sending messages to the
-# ws server
+# TODO: Use a message broker (REDIS or rabbitMQ) for sending messages to the WS Server
 class WSClient(Thread):
     def __init__(self):
         Thread.__init__(self, daemon=True)
@@ -56,11 +55,17 @@ class WSClient(Thread):
         self.loop.run_forever()
 
 
+# TODO: Add tests
 class APIServer(Flask):
+    """
+    Represents a arc! Api Server. It is completely stateless and is designed
+    to be easily run with gunicorn.
+    """
     def __init__(self):
         Flask.__init__(self, __name__)
 
         # Auth Initialization
+        # NOTE: To invalidate all JWT's just change this secret.
         self.auth = Auth(os.environ.get("JWT_SECRET"), "HS256")
 
         # Database initialization
@@ -69,7 +74,6 @@ class APIServer(Flask):
 
         # Register the routes
         # TODO: message rate limit 10 per 5 second
-        # TODO: message character limit: 2000
         self.route("/api", methods=["GET", "POST"])(self.index)
         self.route("/api/v1/myInfo", methods=["GET"])(self.my_info)
         self.route("/api/v1/messages", methods=["POST"])(self.post_message)
@@ -80,169 +84,279 @@ class APIServer(Flask):
         self.ws_client = WSClient()
         self.ws_client.start()
 
-    def index(self):
-        self.ws_client.send_message("hello there. This is index.")
+    # --- HELPER METHODS ---
+    @staticmethod
+    def _validate_json_req(req):
+        """
+        This function makes sure that the request made to the endpoint is of the
+        content type application/json. Why? because our full API is JSON only. So
+        this acts as the first layer of check.
+
+        Parameters
+        ----------
+        req: request
+            The actual request.
+        """
+        try:
+            json_req = req.get_json()
+        except Exception:
+            return {
+                "error": "This is a very bad request :(. Check the API docs on how to use the arc! api."
+            }, 400
+
+        if json_req is None:
+            return {
+                "error": "This endpoint only accepts content type application/json."
+            }, 400
+
+        return json_req, 200
+
+    @staticmethod
+    def _validate_type(parsed_req, input_values):
+        """
+        This function checks the keys in the parsed request and makes sure that they
+        are all of :type:`str`. Why str? Coz' why not? Also with string I have to deal
+        with less headaches.
+
+        Parameters
+        ----------
+        parsed_req: Dict
+            The parsed request that we got by using request.get_json()
+        input_values: list[dict]
+            Of the form [{"name": "", "key": ""}]. The name is used while generating
+            the error messages. key is well the key in `req` that has to be type checked.
+        """
+        for item in input_values:
+            try:
+                if type(parsed_req[item.get("key")]) != str:
+                    return {
+                        "error": "{} can only be a string.".format(item.get("name"))
+                    }, 400
+
+                if len(parsed_req[item.get("key")]) == 0:
+                    return {
+                        "error": "{} can't be blank.".format(item.get("name"))
+                    }, 400
+
+            except KeyError:
+                return {
+                    "error": "{} is not present.".format(item.get("name"))
+                }, 400
+
+        return None, 200
+
+    def _verify(self, req):
+        """
+        This function makes sure that the Authorization header is present and
+        decodes the JWT.
+
+        Parameters
+        ----------
+        req: request
+            The actual request.
+        """
+        try:
+            auth_header = req.headers["authorization"]
+        except KeyError:
+            return {
+                "error": "Missing Authorization header."
+            }, 400
+
+        decoded = self.auth.decode_token(auth_header[7:])
+        if not decoded:
+            return {
+                "error": "Invalid token."
+            }, 401
+
+        return decoded, 200
+
+    # --- ENDPOINTS START FROM HERE ---
+    @staticmethod
+    def index():
         return jsonify({
-            "hello": "api is working properly"
+            "hello": "arc! API seems to be working properly."
         }), 200
 
     # TODO: proper exception handling for this route
     def my_info(self):
-        try:
-            auth_header = request.headers.get("authorization")
-            decoded = self.auth.decode_token(auth_header[7:])
-            if decoded:
-                user_info = self.db.get_user_info(decoded.get("email"))
-                print(user_info)
-                return jsonify({
+        # --- Verify Token ---
+        # If there were no errors while decoding the JWT, then
+        # data will be the decoded JWT data. If there were error
+        # encountered, like missing headers, expired JWT etc. ,
+        # then data will the corresponding error message.
+        data, code = self._verify(request)
+        if code == 200:
+            user_info = self.db.get_user_info(data["email"])
+            print(user_info)
+            return jsonify({
+                "id": user_info[0],
+                "username": user_info[1],
+                "avatar": user_info[2]
+            }), code
+
+        else:
+            return jsonify(data), code
+
+    def post_message(self):
+        # --- Validate JSON ---
+        # Note: if the request is valid json then it
+        # req will be the parsed JSON. Else req is the
+        # error message generated by :meth:_validate_json_req
+        req, code = self._validate_json_req(request)
+        if code != 200:
+            return jsonify(req), code
+
+        #  --- Validate Type ---
+        err, code = self._validate_type(req, [{
+            "name": "message content",
+            "key": "content"
+        }])
+        if code != 200:
+            return jsonify(err), code
+
+        del err
+
+        # --- Limit message size to 2000 characters ---
+        if len(req["content"]) > 2000:
+            return jsonify({
+                "error": "Message content can't be more than 2000 characters."
+            }), 400
+
+        #  --- Verify Token ---
+        # If there were no errors while decoding the JWT, then
+        # data will be the decoded JWT data. If there were error
+        # encountered, like missing headers, expired JWT etc. ,
+        # then data will the corresponding error message.
+        data, code = self._verify(request)
+        if code == 200:
+            # TODO: this HAS TO BE CACHED somehow.
+            # Hammering the db for every single message won't do.
+            user_info = self.db.get_user_info(data["email"])
+            print(user_info)
+
+            self.ws_client.send_message(json.dumps({
+                "type": "POST_MESSAGE",
+                "content": req["content"],
+                "timestamp": datetime.utcnow().isoformat(),
+                "author": {
                     "id": user_info[0],
                     "username": user_info[1],
                     "avatar": user_info[2]
-                }), 200
+                }
+            }))
 
-            else:
-                return jsonify({
-                    "error": "Invalid token."
-                }), 401
-
-        except KeyError:
             return jsonify({
-                "error": "Missing Authorization header."
-            }), 400
-
-    def post_message(self):
-        try:
-            req = request.get_json()
-            if req is None:
-                return jsonify({
-                    "error": "This endpoint only accepts content type application/json."
-                }), 400
-
-            content = req.get("content")
-            auth_header = request.headers.get("authorization")
-            decoded = self.auth.decode_token(auth_header[7:])
-
-            if decoded:
-                # TODO: this HAS TO BE CACHED somehow.
-                # Hammering the db for every single message won't do.
-                user_info = self.db.get_user_info(decoded.get("email"))
-                print(user_info)
-
-                self.ws_client.send_message(json.dumps({
-                    "type": "POST_MESSAGE",
-                    "content": content,
-                    "timestamp": datetime.utcnow().isoformat(),
-                    "author": {
-                        "id": user_info[0],
-                        "username": user_info[1],
-                        "avatar": user_info[2]
-                    }
-                }))
-
-                return jsonify({
-                    "content": content,
-                    "timestamp": datetime.utcnow().isoformat(),
-                    "author": {
-                        "id": user_info[0],
-                        "username": user_info[1],
-                        "avatar": user_info[2]
-                    }
-                }), 200
-
-            else:
-                return jsonify({
-                    "error": "Invalid token."
-                }), 401
-
-        except KeyError:
-            return jsonify({
-                "error": "message content is not present."
-            }), 400
+                "content": req["content"],
+                "timestamp": datetime.utcnow().isoformat(),
+                "author": {
+                    "id": user_info[0],
+                    "username": user_info[1],
+                    "avatar": user_info[2]
+                }
+            }), code
+        else:
+            return jsonify(data), code
 
     #  --- Authentication / Registration Routes ---
+    # TODO: tokens should not be handed out so frequently
     def login(self):
+        # --- Validate JSON ---
+        # Note: if the request is valid json then it
+        # req will be the parsed JSON. Else req is the
+        # error message generated by :meth:_validate_json_req
+        req, code = self._validate_json_req(request)
+        if code != 200:
+            return jsonify(req), code
+
+        #  --- Validate Type ---
+        err, code = self._validate_type(req, [{
+            "name": "email",
+            "key": "email"
+        }, {
+            "name": "password",
+            "key": "password"
+        }])
+        if code != 200:
+            return jsonify(err), code
+
+        del err
+
+        # --- Code ---
+        print("Login attempt: {} | {}".format(req["email"], req["password"]))
         try:
-            # print("Request: --> ", request)
-            # FIXME: edge case where content-type is application/json but body is blank
-            req = request.get_json()
-            if req is None:
+            if self.db.verify_user(req["email"], req["password"]):
                 return jsonify({
-                    "error": "This endpoint only accepts content type application/json."
-                }), 400
+                    "token": self.auth.generate_token(req["email"], req["password"])
+                }), 200
 
-            email = req["email"]
-            password = req["password"]
-
-            # First layer of validation before hitting
-            # the database abstraction api.
-            if len(email) == 0 or len(password) == 0:
-                raise KeyError
-
-            print("Login attempt: {} | {}".format(email, password))
-            try:
-                if self.db.verify_user(email, password):
-                    return jsonify({
-                        "token": self.auth.generate_token(email, password)
-                    }), 200
-
-                else:
-                    return jsonify({
-                        "error": "Login failed. Are you sure the password is correct?"
-                    }), 401
-
-            except NoUserError as e:
+            else:
                 return jsonify({
-                    "error": e.message
-                }), 404
+                    "error": "Login failed. Are you sure the password is correct?"
+                }), 401
 
-        except KeyError:
+        except ValidationError as e:
             return jsonify({
-                "error": "Email and password cant be missing or blank."
+                "error": e.message
             }), 400
 
-    def register(self):
-        try:
-            req = request.get_json()
-            if req is None:
-                return jsonify({
-                    "error": "This endpoint only accepts content type application/json."
-                }), 400
-
-            username = req["username"]
-            email = req["email"]
-            password = req["password"]
-
-            # First layer of validation before hitting
-            # the database abstraction api.
-            if len(username) == 0 or len(email) == 0 or len(password) == 0:
-                raise KeyError
-
-            print("Register attempt: {} | {}".format(email, password))
-            try:
-                # Returns true if the query executed
-                if self.db.create_user(username, email, password):
-                    return jsonify({
-                        "token": self.auth.generate_token(email, password)
-                    }), 200
-
-                # And false if it failed due to integrity error which in
-                # our case means this email id already exists.
-                else:
-                    return jsonify({
-                        "error": "This email-id already exists."
-                    }), 400
-
-            except ValidationError as e:
-                return jsonify({
-                    "error": e.message
-                }), 400
-
-            except LengthError as e:
-                return jsonify({
-                    "error": e.message
-                }), 400
-
-        except KeyError:
+        except LengthError as e:
             return jsonify({
-                "error": "Username, email and password can't be missing or blank."
+                "error": e.message
+            }), 400
+
+        except NoUserError as e:
+            return jsonify({
+                "error": e.message
+            }), 404
+
+    # TODO: tokens should not be handed out so frequently
+    def register(self):
+        # --- Validate JSON ---
+        # Note: if the request is valid json then it
+        # req will be the parsed JSON. Else req is the
+        # error message generated by :meth:_validate_json_req
+        req, code = self._validate_json_req(request)
+        if code != 200:
+            return jsonify(req), code
+
+        #  --- Validate Type ---
+        err, code = self._validate_type(req, [{
+            "name": "username",
+            "key": "username"
+        }, {
+            "name": "email",
+            "key": "email"
+        }, {
+            "name": "password",
+            "key": "password"
+        }])
+        if code != 200:
+            return jsonify(err), code
+
+        del err
+
+        # --- Code ---
+        print("Register attempt: {} | {} | {}".format(req["username"], req["email"], req["password"]))
+        try:
+            # Returns true if the query executed
+            if self.db.create_user(req["username"], req["email"], req["password"]):
+                return jsonify({
+                    "token": self.auth.generate_token(req["email"], req["password"])
+                }), 200
+
+            # And false if it failed due to integrity error which in
+            # our case means this email id already exists.
+            else:
+                return jsonify({
+                    "error": "This email-id already exists."
+                }), 400
+
+        except ValidationError as e:
+            return jsonify({
+                "error": e.message
+            }), 400
+
+        except LengthError as e:
+            return jsonify({
+                "error": e.message
             }), 400
